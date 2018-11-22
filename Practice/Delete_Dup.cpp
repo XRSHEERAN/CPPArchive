e99796ec77ca46f1b1e086a1e3c25dbf68fc4be4
Xianrun QU <xqu25@wisc.edu>
2018-11-21 21:05:53 -0600

    Create Delete_Dup.cpp

---
+++ Practice/Delete_Dup.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL)
+            return NULL;
+        ListNode *curr=head;
+        
+        while(curr->next){
+            if(curr->val==curr->next->val){
+                curr->next=curr->next->next;
+            }
+            else
+                curr=curr->next;
+        }
+        return head;
+    }
+};
